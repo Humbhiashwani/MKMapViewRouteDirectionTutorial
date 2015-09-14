943913caba5e6cb98abf26e5300b5dcbaa96ffdf
MaitrayeeGhosh <maitrayeeghosh89@gmail.com>
2015-09-14 22:19:06 +0530

    Initial Commit

---
+++ MKMapViewRouteDirectionTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MKMapViewRouteDirectionTutorial
+//
+//  Created by Maitrayee Ghosh on 14/09/15.
+//  Copyright (c) 2015 Maitrayee Ghosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MKMapViewRouteDirectionTutorial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MKMapViewRouteDirectionTutorial
+//
+//  Created by Maitrayee Ghosh on 14/09/15.
+//  Copyright (c) 2015 Maitrayee Ghosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
